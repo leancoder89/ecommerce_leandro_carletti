e4ba3d4765325379b833381f4c223fa7c763e9b0
Leandro Carletti <carletti.l.n@gmail.com>
2024-01-20 18:20:22 -0300

    creacion de .env

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDzhgX5vdI_xm6DEwjRyFt5rSlSyqh_iJQ",
-    authDomain: "yerba-mate-store-lean-carletti.firebaseapp.com",
-    projectId: "yerba-mate-store-lean-carletti",
-    storageBucket: "yerba-mate-store-lean-carletti.appspot.com",
-    messagingSenderId: "946710403683",
-    appId: "1:946710403683:web:caa9ed6d38b7682f497656"
+    apiKey: import.meta.env.VITE_apiKey ,
+    authDomain: import.meta.env.VITE_authDomain ,
+    projectId: import.meta.env.VITE_projectId ,
+    storageBucket: import.meta.env.VITE_storageBucket ,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+    appId: import.meta.env.VITE_appId
 };
 
 
